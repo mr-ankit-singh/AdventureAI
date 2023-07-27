4b5907072758f16f8d62801bc58ce36dc428e343
Ankit Singh <root@gmail.com>
2023-07-27 18:10:14 +0530

    Initial Commit

---
+++ app/(dashboard)/settings/page.tsx
@@ -0,0 +1,28 @@
+import { Settings } from "lucide-react";
+
+import { Heading } from "@/components/heading";
+
+const SettingsPage = async () => {
+  
+
+  return ( 
+    <div>
+      <Heading
+        title="Settings"
+        description="Manage account settings."
+        icon={Settings}
+        iconColor="text-gray-700"
+        bgColor="bg-gray-700/10"
+      />
+      <div className="px-4 lg:px-8 space-y-4">
+        <div className="text-muted-foreground text-sm">
+          {false ? "You are currently on a Pro plan." : "You are currently on a free plan."}
+        </div>
+        
+      </div>
+    </div>
+   );
+}
+ 
+export default SettingsPage;
+
---
+++ constants.ts
@@ -0,0 +1,27 @@
+import { Code, ImageIcon, MessageSquare, Music, VideoIcon } from "lucide-react";
+
+export const MAX_FREE_COUNTS = 5;
+
+export const tools = [
+  {
+    label: 'Conversation',
+    icon: MessageSquare,
+    href: '/conversation',
+    color: "text-violet-500",
+    bgColor: "bg-violet-500/10",
+  },
+  {
+    label: 'Image Generation',
+    icon: ImageIcon,
+    color: "text-pink-700",
+    bgColor: "bg-pink-700/10",
+    href: '/image',
+  },
+  {
+    label: 'Code Generation',
+    icon: Code,
+    color: "text-green-700",
+    bgColor: "bg-green-700/10",
+    href: '/code',
+  },
+];
